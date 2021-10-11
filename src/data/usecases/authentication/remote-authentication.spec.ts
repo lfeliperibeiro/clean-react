91aec46efa6b64e764fc14fba874d23f4338c754
Felipe Ribeiro <piporibeiro@gmail.com>
2021-10-11 20:26:39 -0300

    refactor: change httpPostClient to receive an object

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -17,7 +17,7 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 }
 
 describe('RemoteAuthentication', () => {
-  it('should  call httpPostClient  with correct URL', async () => {
+  it('should call httpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
