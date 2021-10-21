c3e4b1b6111740088b231069a97146584e97b38e
Felipe Ribeiro <piporibeiro@gmail.com>
2021-10-20 22:48:52 -0300

    test: ensure required field validation returns error if field is empty

---
+++ src/validation/required-field/reqiured-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { RequiredFieldError } from '@/validation/errors'
+
+describe('RequiredFieldValidation', () => {
+  it('should  return error if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+})
