1d0c4ee143b683da6adf9309b689628d7bb39250
Felipe Ribeiro <piporibeiro@gmail.com>
2021-10-22 19:39:15 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import Login from '@/presentation/pages/Login/login'
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote.authentication'
-import { AxiosHttpAdapter } from '@/infra/http/axios-http-client/axios-http-client'
-import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = 'http://fordevs.herokuapp.com/api/login'
-  const axiosHttpClient = new AxiosHttpAdapter()
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
-  const validationComposite = ValidationComposite.build([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
   return (
     <Login
-      validation={validationComposite}
-      authentication={remoteAuthentication}
+      validation={makeLoginValidation()}
+      authentication={makeRemoteAuthentication()}
     />
   )
 }
